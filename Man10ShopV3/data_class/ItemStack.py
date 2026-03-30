695774f87b1b762b84a8ca71e28dbb5ce676cce1
Jin Morikawa <forest611328@gmail.com>
2026-03-30 16:56:32 +0900

    ItemStackのハードコード部分修正

--- Man10ShopV3/data_class/ItemStack.py
+++ Man10ShopV3/data_class/ItemStack.py
@@ -1,10 +1,10 @@
 class ItemStack(object):
-    type_base64: str = "rO0ABXcEAAAAAXNyABpvcmcuYnVra2l0LnV0aWwuaW8uV3JhcHBlcvJQR+zxEm8FAgABTAADbWFwdAAPTGphdmEvdXRpbC9NYXA7eHBzcgA1Y29tLmdvb2dsZS5jb21tb24uY29sbGVjdC5JbW11dGFibGVNYXAkU2VyaWFsaXplZEZvcm0AAAAAAAAAAAIAAlsABGtleXN0ABNbTGphdmEvbGFuZy9PYmplY3Q7WwAGdmFsdWVzcQB+AAR4cHVyABNbTGphdmEubGFuZy5PYmplY3Q7kM5YnxBzKWwCAAB4cAAAAAN0AAI9PXQAAXZ0AAR0eXBldXEAfgAGAAAAA3QAHm9yZy5idWtraXQuaW52ZW50b3J5Lkl0ZW1TdGFja3NyABFqYXZhLmxhbmcuSW50ZWdlchLioKT3gYc4AgABSQAFdmFsdWV4cgAQamF2YS5sYW5nLk51bWJlcoaslR0LlOCLAgAAeHAAAAqqdAAHRElBTU9ORA=="
-    type_md5: str = "f955adf1e7104a74953d377f4b039b52"
+    type_base64: str = "H4sIAAAAAAAA/+NiYGBm4HZJLEkMSy0qzszPY2AQjOBgYMpMYRDKzcxLTS5KTCuxyk/Mji/OTM9jZmBNzi/NK2FgYGBkAAC/0pwVOwAAAA=="
+    type_md5: str = "e5314c9cba92af630f609899088a12ab"
     amount: int = 1
     display_name: str = None
     lore: list = []
-    material: str = "DIAMOND"
+    material: str = "OAK_SIGN"
     custom_model_data = int = -1
 
     def from_json(self, data: dict):
